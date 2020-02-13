c494e6f5e68e0641cdf68916bfc58d3bebb95fe1
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-02-13 09:23:59 +0900

    (Cake_IFM11/WS) periodical updates

---
+++ commands/2-1)_move-files-in-iphone-dir-to-WS_Cake_IFM11.bat
@@ -0,0 +1,11 @@
+REM ****************************
+REM 	web page : http://localhost/Eclipse_Luna/Cake_IFM11/images/image_manager
+
+REM ****************************
+
+C:\WORKS_2\WS\WS_Cake_IFM11\commands\18)_move-mov-files_to-ifm11-folder.bat && C:\WORKS_2\WS\WS_Cake_IFM11\get_folder_list.[v.2].20190210_091405.rb
+REM C:\WORKS_2\WS\WS_Cake_IFM11\get_folder_list.[v.2].20190210_091405.rb
+
+
+
+REM exit
---
+++ commands/2-2)_move-files-back-to-tmp-dir.bat
@@ -0,0 +1,10 @@
+REM ****************************
+REM 	web page : http://localhost/Eclipse_Luna/Cake_IFM11/images/image_manager
+
+REM ****************************
+
+C:\WORKS_2\WS\WS_Cake_IFM11\commands\17)_move-mov-files_back-to-iphone-folder.bat
+
+
+
+REM exit
