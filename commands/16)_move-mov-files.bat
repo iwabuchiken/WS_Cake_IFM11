32b4a1b514803c9944f451d43e857620eb0412ea
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-04-12 10:10:42 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/16)_move-mov-files.bat
+++ commands/16)_move-mov-files.bat
@@ -1,8 +1,22 @@
-echo starting...
+echo starting... 16)_move-mov-files.bat
 
+REM **************************
+REM 	mov
+REM **************************
 echo move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\*.mov C:\WORKS_2\WS\WS_Cake_IFM11\movies_WS_Cake_IFM11\2019
+
 move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\*.mov C:\WORKS_2\WS\WS_Cake_IFM11\movies_WS_Cake_IFM11\2019
 
+REM **************************
+REM 	mp4
+REM **************************
+echo move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11\movies_WS_Cake_IFM11\2019
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11\movies_WS_Cake_IFM11\2019
+
+REM **************************
+REM 	m4a
+REM **************************
 echo move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\*.m4a C:\WORKS_2\WS\WS_Cake_IFM11\voice-memos
 move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\*.m4a C:\WORKS_2\WS\WS_Cake_IFM11\voice-memos
 
