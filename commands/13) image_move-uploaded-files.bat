0bdce71a9a808554ae2cfbbb3b4c6a970ea5688c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-05-05 09:39:58 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/13) image_move-uploaded-files.bat
+++ commands/13) image_move-uploaded-files.bat
@@ -5,7 +5,16 @@ REM 	dir : C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others
 REM ref multiple : lines https://stackoverflow.com/questions/69068/long-commands-split-over-multiple-lines-in-windows-vista-batch-bat-file
 REM move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\*^
 REM 	 C:\WORKS_2\WS\WS_Cake_IFM11\iphone_upload_done\
-
+REM **************************
+REM 	jpeg
+REM **************************
 move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\*.*g C:\WORKS_2\WS\WS_Cake_IFM11\iphone_upload_done\
-	 
+
+REM **************************
+REM 	mid
+REM 	2020”N5ŒŽ5“ú0:22:36
+REM **************************
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\*.mid C:\WORKS_2\WS\WS_Cake_IFM11\audio-files
+
+
 REM pause
