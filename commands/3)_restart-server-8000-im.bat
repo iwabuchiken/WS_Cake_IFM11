7bdffe94b75da2f1ade3ba8d998049acf1d82f03
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-12-03 14:14:36 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/3)_restart-server-8000-im.bat
+++ commands/3)_restart-server-8000-im.bat
@@ -3,7 +3,11 @@ REM 	at : 2020/02/13 08:27:16
 REM 	edited : 2020”N6ŒŽ10“ú9:57:03
 REM ****************************
 
-start C:\WORKS_2\"Command Prompt.lnk" /k "C:\WORKS_2\batches\r.bat d2"
+rem start C:\WORKS_2\"Command Prompt.lnk" /k "C:\WORKS_2\batches\r.bat d2"
+
+rem : updated : 2020”N12ŒŽ2“ú17:39:30
+rem start && r d2
+start r d2
 
 REM C:\WORKS_2\batches\r.bat d2
 
