1f800a5b0f1c28410d7bfc1ce6ac61dbb62701cb
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-01-31 08:37:57 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/1) change_file_names.bat
+++ commands/1) change_file_names.bat
@@ -1,5 +1,16 @@
 @echo off
 
+rem =======================
+rem 	mid files ==> gen list
+rem 	2021”N1ŒŽ31“ú7:42:45
+rem =======================
+
+"C:\WORKS_2\WS\WS_Cake_IFM11\commands\2-0) edit_memos.9-0_mid-files.bat"
+
+rem =======================
+rem 	change file names
+rem 	2021”N1ŒŽ31“ú7:42:45
+rem =======================
 pushd C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11
 
 echo changing names...
