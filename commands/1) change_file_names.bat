8eca83c31dd04116e8c3f8e8317ad848f1afc349
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-02-01 15:40:43 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/1) change_file_names.bat
+++ commands/1) change_file_names.bat
@@ -5,7 +5,7 @@ rem 	mid files ==> gen list
 rem 	2021”N1ŒŽ31“ú7:42:45
 rem =======================
 
-"C:\WORKS_2\WS\WS_Cake_IFM11\commands\2-0) edit_memos.9-0_mid-files.bat"
+call "C:\WORKS_2\WS\WS_Cake_IFM11\commands\2-0) edit_memos.9-0_mid-files.bat"
 
 rem =======================
 rem 	change file names
