214a959a76825fd9fa5a0be7805c559bbbd26270
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-02-11 12:35:50 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/2-0) edit_memos.9-0_mid-files.bat
+++ commands/2-0) edit_memos.9-0_mid-files.bat
@@ -6,6 +6,9 @@ rem 	2. the gen-ed list ==> used in memo-writing for came_ifm tasks
 REM ****************************
 copy C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.mid C:\WORKS_2\WS\WS_Cake_IFM11
 
+rem : m4a files : 2021”N2ŒŽ11“ú10:14:10
+copy C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.m4a C:\WORKS_2\WS\WS_Cake_IFM11
+
 C:\WORKS_2\WS\WS_Cake_IFM11\get_folder_list.[v.2].20190210_091405.rb
 
 del C:\WORKS_2\WS\WS_Cake_IFM11\*.mid
