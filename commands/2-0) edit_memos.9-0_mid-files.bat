87ab32a0135fa49e1a58374b6e803a212edfc8e1
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-03-13 13:04:08 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/2-0) edit_memos.9-0_mid-files.bat
+++ commands/2-0) edit_memos.9-0_mid-files.bat
@@ -4,11 +4,22 @@ rem 	<purpose of this file>
 rem 	1. gen a list of mid file names.
 rem 	2. the gen-ed list ==> used in memo-writing for came_ifm tasks
 REM ****************************
+REM ****************************
+rem 	list of extensions
+REM ****************************
+set listof_extensions=*.mid *.m4a *.mp4 *.avi
+
 copy C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.mid C:\WORKS_2\WS\WS_Cake_IFM11
 
 rem : m4a files : 2021”N2ŒŽ11“ú10:14:10
 copy C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.m4a C:\WORKS_2\WS\WS_Cake_IFM11
 
+rem : mp4,avi files : 2021”N3ŒŽ13“ú12:04:24
+copy C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11
+
+copy C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.avi C:\WORKS_2\WS\WS_Cake_IFM11
+
 C:\WORKS_2\WS\WS_Cake_IFM11\get_folder_list.[v.2].20190210_091405.rb
 
-del C:\WORKS_2\WS\WS_Cake_IFM11\*.mid
+rem del C:\WORKS_2\WS\WS_Cake_IFM11\*.mid
+del C:\WORKS_2\WS\WS_Cake_IFM11\%listof_extensions:"=%
