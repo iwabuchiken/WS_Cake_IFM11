09ba52c0acc5493ebd4dee6641bfc588fe0c39f9
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-01-13 00:49:43 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/2-0 -0.1)_close-apps.starter.bat
+++ commands/2-0 -0.1)_close-apps.starter.bat
@@ -1,1 +1,5 @@
-start C:\WORKS_2\Programs\sakura\sakura.exe "C:\WORKS_2\WS\WS_Cake_IFM11\commands\2-0 -0)_close-apps.txt"
+rem : c/o : 2022”N1ŒŽ12“ú23:21:40
+rem start C:\WORKS_2\Programs\sakura\sakura.exe "C:\WORKS_2\WS\WS_Cake_IFM11\commands\2-0 -0)_close-apps.txt"
+
+rem : new : 2022”N1ŒŽ12“ú23:21:46
+start C:\WORKS_2\Programs\sakura\sakura.exe "C:\WORKS_2\WS\WS_Cake_IFM11\commands\cake-ifm_commands.txt"
