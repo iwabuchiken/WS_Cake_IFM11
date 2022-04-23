9f09638c2f1e277fc93175fc1b40d1e0a0dd01b6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-04-23 11:50:44 +0900

    (Cake_IFM11/WS) periodical updates

---
+++ commands/11-0_update_date.start.bat
@@ -0,0 +1,9 @@
+rem ==================================
+rem 	2022”N4ŒŽ22“ú
+rem 	used in : C:\WORKS_2\WS\WS_Cake_IFM11\commands\cake-ifm_commands.txt
+rem ==================================
+C:\WORKS_2\Programs\sakura\sakura.exe "C:\WORKS_2\WS\WS_Cake_IFM11\commands\11-0) update date.txt"
+
+exit
+
+rem start "C:\WORKS_2\WS\WS_Cake_IFM11\commands\11-0) update date.txt"
