b85f3aa4ae072e857552fe111337b563ffdb2106
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-05-15 11:53:59 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/1-5_move-files_HD-to-tmp.bat
+++ commands/1-5_move-files_HD-to-tmp.bat
@@ -1,4 +1,7 @@
-move G:\tmp.20220507_191202\* C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+rem move G:\tmp.20220507_191202\* C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move G:\tmp.20220507_191202\*.jpg C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+move G:\tmp.20220507_191202\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11\tmp
 
 pause
 
