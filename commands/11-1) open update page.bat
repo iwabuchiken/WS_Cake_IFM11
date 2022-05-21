be853c198917bca5c734dc95b65510e11753f24f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-05-21 10:55:19 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/11-1) open update page.bat
+++ commands/11-1) open update page.bat
@@ -1,8 +1,23 @@
 rem : 2022”N3ŒŽ2“ú10:04:34
-goto firefox
+goto yandex
+rem goto firefox
 
 rem goto opera
 REM goto chrome
+
+
+rem : yandex // 2022”N5ŒŽ21“ú10:00:44
+:yandex
+set browser="C:\Users\iwabuchiken\AppData\Local\Yandex\YandexBrowser\Application\browser.exe"
+
+set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/admins/edit/3"
+rem set url_2="http://localhost/Eclipse_Luna/Cake_IFM11/images/image_manager"
+
+%browser% %url_1%
+rem %browser% %url_2%
+
+goto end
+
 
 REM ================================
 REM 	open with : opera
