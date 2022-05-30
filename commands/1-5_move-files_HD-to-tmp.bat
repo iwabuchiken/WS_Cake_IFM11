952ffc7b2d76ba0e3bad31e82a10fcb3543cf538
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-05-30 10:57:21 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/1-5_move-files_HD-to-tmp.bat
+++ commands/1-5_move-files_HD-to-tmp.bat
@@ -3,6 +3,10 @@ rem move G:\tmp.20220507_191202\* C:\WORKS_2\WS\WS_Cake_IFM11\tmp
 move G:\tmp.20220507_191202\*.jpg C:\WORKS_2\WS\WS_Cake_IFM11\tmp
 move G:\tmp.20220507_191202\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11\tmp
 
+move G:\tmp.20220507_191202\*.mid C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+move G:\tmp.20220507_191202\*.m4a C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+move G:\tmp.20220507_191202\*.aac C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
 pause
 
 exit
--- commands/1-6_move-files_tmp-to-HD.bat
+++ commands/1-6_move-files_tmp-to-HD.bat
@@ -3,6 +3,10 @@ rem move G:\tmp.20220507_191202\* C:\WORKS_2\WS\WS_Cake_IFM11\tmp
 move C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.jpg G:\tmp.20220507_191202
 move C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.mp4 G:\tmp.20220507_191202
 
+move C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.mid G:\tmp.20220507_191202
+move C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.m4a G:\tmp.20220507_191202
+move C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.aac G:\tmp.20220507_191202
+
 rem move G:\tmp.20220507_191202\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11\tmp
 
 pause
