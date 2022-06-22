970a820d0e2dfd1a876bd052d283425c54154919
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-06-22 12:01:03 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/1-7_start-filezilla.bat
+++ commands/1-7_start-filezilla.bat
@@ -1,5 +1,17 @@
-C:\WORKS_2\batches\s.bat fi
+rem ==========================
+rem 	file : C:\WORKS_2\WS\WS_Cake_IFM11\commands\1-7_start-filezilla.bat
+rem ==========================
+echo  starting...
+echo.
 
-rem pause
+pause
 
-exit
+rem C:\WORKS_2\batches\s.bat fi
+echo "C:\WORKS_2\Programs\FileZilla FTP Client\filezilla.exe"
+
+"C:\WORKS_2\Programs\FileZilla FTP Client\filezilla.exe"
+
+pause
+
+
+rem exit
---
+++ commands/1-7_start-filezilla.version-2.bat
@@ -0,0 +1,23 @@
+rem ==========================
+rem 	file : C:\WORKS_2\WS\WS_Cake_IFM11\commands\1-7_start-filezilla.version-2.bat
+rem 	date : 2022年6月19日15:42:30
+rem ==========================
+
+echo  starting...
+echo.
+
+pause
+
+pushd C:\WORKS_2\batches\
+
+echo dir changed to : %cd%
+
+pause
+
+s.bat fi
+rem C:\WORKS_2\batches\s.bat fi
+
+pause
+
+
+rem exit
