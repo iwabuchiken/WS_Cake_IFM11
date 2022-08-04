c28c985773095f6fcffbb3388b0ff9fafedcad9e
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-08-04 12:35:10 +0900

    (Cake_IFM11/WS) periodical updates

---
+++ commands/1-8.3_move-files_tmp-to-iphone.bat
@@ -0,0 +1,20 @@
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\* C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.jpg C:\WORKS_2\WS\WS_Cake_IFM11\iphone
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11\iphone
+
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.mid C:\WORKS_2\WS\WS_Cake_IFM11\iphone
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\\*.mid C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.m4a C:\WORKS_2\WS\WS_Cake_IFM11\iphone
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\\*.m4a C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\tmp\*.aac C:\WORKS_2\WS\WS_Cake_IFM11\iphone
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\\*.aac C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+pause
+
+exit
---
+++ commands/1-8.4_move-files_iphone-to-tmp.bat
@@ -0,0 +1,20 @@
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\* C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.jpg C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.mid C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\\*.mid C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.m4a C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\\*.m4a C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.aac C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\\*.aac C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+pause
+
+exit
