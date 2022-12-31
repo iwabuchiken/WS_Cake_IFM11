c8718616041b0d1ac4b202328bd3383183e59ae1
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-12-31 13:24:36 +0900

    (Cake_IFM11/WS) periodical updates

---
+++ commands/1-8.4-1_move-files_iphone-to-HD.tmp.bat
@@ -0,0 +1,20 @@
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\* G:\tmp.20220507_191202
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.jpg G:\tmp.20220507_191202
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.mp4 G:\tmp.20220507_191202
+
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\\*.mp4 G:\tmp.20220507_191202
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.mid G:\tmp.20220507_191202
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\\*.mid G:\tmp.20220507_191202
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.m4a G:\tmp.20220507_191202
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\\*.m4a G:\tmp.20220507_191202
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.aac G:\tmp.20220507_191202
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\\*.aac G:\tmp.20220507_191202
+
+pause
+
+exit
