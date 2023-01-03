43f8fdacbfdb19562b716fd703b3ca110d839931
iwabuchiken <iwabuchi.k.2010@gmail.com>
2023-01-03 16:18:48 +0900

    (Cake_IFM11/WS) periodical updates

---
+++ commands/1-8-1_move-files_iphone_to_iphone-to-upload.bat
@@ -0,0 +1,20 @@
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\* C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.jpg C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
+
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.mid C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\\*.mid C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.m4a C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\\*.m4a C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.aac C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
+rem move C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload\\*.aac C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+pause
+
+exit
