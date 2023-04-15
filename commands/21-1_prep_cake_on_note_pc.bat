50b87800bdc0448412047d5379064d4dcb3e0bea
iwabuchiken <iwabuchi.k.2010@gmail.com>
2023-04-15 14:27:07 +0900

    (Cake_IFM11/WS) periodical updates

---
+++ commands/21-1_prep_cake_on_note_pc.bat
@@ -0,0 +1,8 @@
+rem / ref / https://www.howtoguides.org/Force-Copy-Files-In-Windows-And-Fix-Common-Access-Denied-Errors
+xcopy /Y C:\WORKS_2\WS\WS_Cake_IFM11\commands\cake-ifm_commands.txt H:\workspace_cake_ifm
+xcopy /Y C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.ods H:\workspace_cake_ifm
+xcopy /Y C:\WORKS_2\WS\WS_Cake_IFM11\iphone\* H:\workspace_cake_ifm\iphone
+
+rem copy C:\WORKS_2\WS\WS_Cake_IFM11\commands\cake-ifm_commands.txt H:\workspace_cake_ifm
+
+pause
---
+++ commands/21-2_cake_ifm_back_to_desktop.bat
@@ -0,0 +1,7 @@
+xcopy /Y H:\workspace_cake_ifm\cake-ifm_commands.txt  C:\WORKS_2\WS\WS_Cake_IFM11\commands
+xcopy /Y H:\workspace_cake_ifm\entries.ods  C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others
+rem xcopy /Y H:\workspace_cake_ifm\iphone\* C:\WORKS_2\WS\WS_Cake_IFM11\iphone
+
+rem copy C:\WORKS_2\WS\WS_Cake_IFM11\commands\cake-ifm_commands.txt H:\workspace_cake_ifm
+
+pause
