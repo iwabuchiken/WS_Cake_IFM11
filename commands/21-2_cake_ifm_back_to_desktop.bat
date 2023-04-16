374fb7de3f9542dbf4216f07e7056c86e21b1145
iwabuchiken <iwabuchi.k.2010@gmail.com>
2023-04-16 10:29:40 +0900

    (Cake_IFM11/WS) periodical updates

--- commands/21-2_cake_ifm_back_to_desktop.bat
+++ commands/21-2_cake_ifm_back_to_desktop.bat
@@ -1,6 +1,12 @@
-xcopy /Y H:\workspace_cake_ifm\cake-ifm_commands.txt  C:\WORKS_2\WS\WS_Cake_IFM11\commands
-xcopy /Y H:\workspace_cake_ifm\entries.ods  C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others
-rem xcopy /Y H:\workspace_cake_ifm\iphone\* C:\WORKS_2\WS\WS_Cake_IFM11\iphone
+@echo off
+rem / cake-ifm_commands.txt / 2023年4月16日9:56:00
+xcopy H:\workspace_cake_ifm\cake-ifm_commands.txt  C:\WORKS_2\WS\WS_Cake_IFM11\commands
+rem xcopy /Y H:\workspace_cake_ifm\cake-ifm_commands.txt  C:\WORKS_2\WS\WS_Cake_IFM11\commands
+
+rem / entries.ods / 2023年4月16日9:56:00
+xcopy H:\workspace_cake_ifm\entries.ods  C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others
+rem xcopy /Y H:\workspace_cake_ifm\entries.ods  C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others
+rem xcopy /Y H:\workspace_cake_ifm\iphone\* C:\WORKS_2\WS\WS_Cake_IFM11\iphonez
 
 rem copy C:\WORKS_2\WS\WS_Cake_IFM11\commands\cake-ifm_commands.txt H:\workspace_cake_ifm
 
---
+++ commands/21-3_cake_ifm_DELETE_IMAGE_FILES_in_HD.bat
@@ -0,0 +1,5 @@
+@echo off
+rem / 2023年4月16日10:03:31
+del H:\workspace_cake_ifm\iphone\*
+
+pause
