d488582c6fabf52e04b983948a88045a92803f08
iwabuchiken <iwabuchi.k.2010@gmail.com>
2023-04-17 22:41:50 +0900

    (Cake_IFM11/WS) periodical updates

---
+++ commands/21-2-2_start_entries_txt_file.bat
@@ -0,0 +1,13 @@
+rem ============================
+rem 
+rem 	created		: 2023年4月17日22:24:31
+rem 	this file	: C:\WORKS_2\WS\WS_Cake_IFM11\commands\21-2-2_start_entries_txt_file.bat
+rem 
+rem ============================
+@echo off
+
+start C:\WORKS_2\WS\WS_Cake_IFM11\iphone && start C:\WORKS_2\WS\WS_Cake_IFM11\commands\__2_ && C:\WORKS_2\batches\s.bat c1
+rem C:\WORKS_2\batches\s.bat c && C:\WORKS_2\batches\s.bat c1
+
+
+pause
