1758244adbcce7d7b8d8fe7e9c6f9c8a753259a2
iwabuchiken <iwabuchi.k.2010@gmail.com>
2023-09-29 18:06:22 +0900

    (Cake_IFM11/WS) periodical updates

---
+++ commands/1-8-2_move-files_iphone_to_HD-MyPassport.bat
@@ -0,0 +1,20 @@
+rem move H:\images\2023\* C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.jpg H:\images\2023
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.mp4 H:\images\2023
+
+rem move H:\images\2023\\*.mp4 C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.mid H:\images\2023
+rem move H:\images\2023\\*.mid C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.m4a H:\images\2023
+rem move H:\images\2023\\*.m4a C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\*.aac H:\images\2023
+rem move H:\images\2023\\*.aac C:\WORKS_2\WS\WS_Cake_IFM11\tmp
+
+pause
+
+exit
